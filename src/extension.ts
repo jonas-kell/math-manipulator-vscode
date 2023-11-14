4ae8f2b276852dad5ff456a476ef60824599b393
Jonas <j.kell@liwetec.com>
2023-11-14 13:39:17 +0100

    fix: exclude imported files from ts linting

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,24 @@ export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand("math-manipulator.helloWorld", () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        vscode.window.showInformationMessage("Hello World from math-manipulator!");
+        // vscode.window.showInformationMessage("Hello World from math-manipulator!");
+
+        const panel = vscode.window.createWebviewPanel("catCoding", "", vscode.ViewColumn.One, {});
+
+        const updateWebview = () => {
+            panel.title = "Cat Coding";
+            panel.webview.html = getWebviewContent();
+        };
+        updateWebview();
+
+        panel.onDidDispose(
+            () => {
+                // When the panel is closed, cancel any future updates to the webview content
+                // TODO
+            },
+            null,
+            context.subscriptions
+        );
     });
 
     context.subscriptions.push(disposable);
@@ -23,3 +40,17 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+function getWebviewContent() {
+    return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Cat Coding</title>
+  </head>
+  <body>
+      <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+  </body>
+  </html>`;
+}
